b08ffbe17e2d8b10e8dd62a88960c208e7d47575
Eberhofer <eberhofer@gmx.ch>
2016-09-19 21:55:03 +0200

    added supporting methods for the creation of strategies. TODO: refactor to tictactoe class and write AI strategies.

--- Tests/test_tictactoe.py
+++ Tests/test_tictactoe.py
@@ -1,19 +1,52 @@
 # import pyytest
-from TicTacToe import tictactoe
+from TicTacToe.tictactoe import winners, check_win, winning_combinations
+from TicTacToe.tictactoe import reduce_winners
+
+winning_set = winners()
 
 def test_winners():
-    testwinners = [{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}]
-    assert tictactoe.winners() == testwinners
+    testwinners = [{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {1, 4, 7}, {2, 5, 8},
+                   {3, 6, 9}, {1, 5, 9}, {3, 5, 7}]
+    assert winning_set == testwinners
 
 def test_check_win_empty():
     p1 = set()
-    assert tictactoe.check_win(p1) == False
+    assert check_win(p1) == False
 
 def test_check_win_loser():
-    p1 = {0,1,3,5,7,8}
-    assert tictactoe.check_win(p1) == False
-
+    p1 = {1, 3, 4, 5, 8}
+    assert check_win(p1) == False
 
 def test_check_win_winner():
-    p1 = {0,1,3,5,6,8}
-    assert tictactoe.check_win(p1) == True
+    p1 = {1, 3, 5, 6, 7, 8}
+    assert check_win(p1) == True
+
+def test_winning_combinations_empty():
+    assert winning_combinations(set(), winning_set) == set()
+
+def test_winning_combinations_19():
+    assert winning_combinations({1, 9}, winning_set) == {3, 7}
+
+def test_winning_combinations_15():
+    assert winning_combinations({1, 5}, winning_set) == {2, 3, 4, 7}
+
+def test_winning_combinations_15_reduced():
+    reducedwinners = [{4, 5, 6}, {7, 8, 9}, {1, 4, 7}, {1, 5, 9}]
+    assert winning_combinations({1, 5}, reducedwinners) == {4}
+
+def test_reduce_winners_winners_23():
+    p1 = {2, 3}
+    reducedwinners = [{4, 5, 6}, {7, 8, 9}, {1, 4, 7}, {1, 5, 9}]
+    assert reduce_winners(winning_set, p1) == reducedwinners
+
+def test_reduce_winners_winners_137():
+    p1 = {1, 3, 7}
+    assert reduce_winners(winning_set, p1) == [{4, 5, 6}, {2, 5, 8}]
+
+def test_reduce_winners_no_winners():
+    p1 = {1, 3, 5, 7}
+    assert reduce_winners(winning_set, p1) == []
+
+def test_reduce_winners_state_empty():
+    p1 = set()
+    assert reduce_winners(winning_set, p1) == winning_set
--- TicTacToe/tictactoe.py
+++ TicTacToe/tictactoe.py
@@ -1,9 +1,37 @@
 def winners():
-    return [{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}]
+    """
+        Returns a list of sets with winning combinations for tic tac toe.
+    """
+    return [{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {1, 4, 7}, {2, 5, 8},
+                   {3, 6, 9}, {1, 5, 9}, {3, 5, 7}]
 
 def check_win(p):
+    """
+        Returns whether a set of occupied tic tac toe fields is a winner.
+    """
     for i in winners():
         if i < p:
-            return True
+            win = True
+            break
     else:
-        return False
+        win = False
+    return win
+
+def reduce_winners(current_winners, opponent_state):
+    return [_ for _ in current_winners if _.isdisjoint(opponent_state)]
+
+def winning_combinations(player_state, current_winners):
+    """
+        Returns a list of numbers that can be played which will give the player
+        with plyer_state a certain winning combination. A certain winning
+        combination is a combination wich has an intersection with at least two
+        sets from current_winners, where the third member differs.
+    """
+    candlist = []
+    for candidate in current_winners:
+        if candidate.isdisjoint(player_state):
+            continue
+        for x in candidate - player_state:
+            candlist.append(x)
+            print(candlist)
+    return {_ for _ in candlist if candlist.count(_) > 1}
